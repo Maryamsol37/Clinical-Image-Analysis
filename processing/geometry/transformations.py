0e478d07297789b11ddb1bdb17884fd351b3dde1
farahahmed27 <farahahmedkamal2004@gmail.com>
2026-05-04 20:13:12 +0300

    feat(transformations):shear is implemented

--- processing/geometry/transformations.py
+++ processing/geometry/transformations.py
@@ -14,3 +14,15 @@ def rotate(image, angle_degrees):
             y_src= -dx * np.sin(angle_radian) + dy * np.cos(angle_radian) + cy
             output[y_out, x_out]= zoom_image(image, x_src, y_src)
     return output
+
+
+def shear(image,shear_x=0.0, shear_y=0.0):
+    h,w=image.shape[:2]
+    output= np.zeros_like(image)
+
+    for y_out in range(h):
+        for x_out in range(w):
+            x_src= x_out - shear_x * y_out
+            y_src= y_out - shear_y * x_out
+            output[y_out, x_out]= zoom_image(image, x_src, y_src)
+    return output
